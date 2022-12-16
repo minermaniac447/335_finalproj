a57187bfd1d96ed7365f1643197aa61fd8bb38fa
minermaniac447 <jwh447@gmail.com>
2022-12-15 19:28:41 -0500

    Clean stuff up, add most final functionality

--- triviaRunner.js
+++ triviaRunner.js
@@ -27,10 +27,7 @@ app.get("/", (request, response) => {
 
 // Submitting username/starting
 app.post("/", async (request, response) => {
-  // PLACEHOLDER, REPLACE WITH RETRIEVAL FROM API
-  let apiSessionToken = "8ce18e8ab03847ba1e59a8c8984f305e303e654da208f18f02f8dcf83c2bd9aa";
   let record = {
-    token: apiSessionToken,
     current_score: 0
   };
 
@@ -49,27 +46,19 @@ app.post("/", async (request, response) => {
 
 // Loading in question/sending it to DB to hold
 app.get("/question", async (request, response) => {
-  // get username and token
+  // get username
   const username = request.cookies.username;
-  const {token} = await client.db(DB).collection(COLLECTION)
-  .findOne({username: username});
 
-  // PLACEHOLDER QUESTION - FROM API REQUEST I NEED "question", "correct_answer", and "incorrect_answers"
   const localResp = await fetch("https://opentdb.com/api.php?amount=1");
   var data = await localResp.json();
-  let {category, question, difficulty, correct_answer, incorrect_answers} = data.results[0];
-  // response.render("testingIndex", {name: question});
+  let {question, correct_answer, incorrect_answers} = data.results[0];
 
-
-  // let apiResponse = JSON.parse('{"category":"Entertainment: Video Games","type":"multiple","difficulty":"easy","question":"In what year was Hearthstone released?","correct_answer":"2014","incorrect_answers":["2011","2013","2012"]}');
-  // let {question, correct_answer, incorrect_answers} = apiResponse;
   await client.db(DB).collection(COLLECTION)
   .updateOne({username: username}, {$set: {question, correct_answer}});
 
   let shuffled_answers = [correct_answer].concat(incorrect_answers);
   shuffled_answers.sort((a, b) => Math.random() - 0.5);
   response.render("question", {question: question, answers: shuffled_answers});
-  // response."render("question");
 });
 
 // Showing the correct answer and the selected answer to the question
@@ -90,7 +79,6 @@ app.post("/answer", async (request, response) => {
     await client.db(DB).collection(COLLECTION).updateOne({username: username}, {$set: update});
   }
 
-  // Need to work out next button logic for if the user correctly or incorrectly answers the question
   response.render("answer", {selected: request.body.selected, correct_answer: correct_answer, question: question, current_score: current_score, correct: correct});
 });
 
@@ -121,77 +109,3 @@ app.get("/leaderboard", async (request, response) => {
 });
 
 app.listen(port);
-
-
-
-// EVERYTHING HERE ON DOWN IS SUMMERCAMP, NOT NEEDED DIRECTLY BUT MAY BE HELPFUL FOR CODE REUSE
-app.get("/apply", (request, response) => {
-  response.render("sendApp", {});
-});
-
-app.post("/processApplication", async (request, response) => {
-  let applicant = {name: request.body.name, email: request.body.email, gpa: Number(request.body.gpa), bginfo: request.body.bginfo ?? ""};
-  await client.db(databaseAndCollection.db)
-  .collection(databaseAndCollection.collection)
-  .insertOne(applicant);
-  response.render("appConf", applicant);
-});
-
-app.get("/reviewApplication", (request, response) => {
-  response.render("reviewApp", {});
-});
-
-app.post("/processReviewApplication", async (request, response) => {
-  let result = await client.db(databaseAndCollection.db)
-  .collection(databaseAndCollection.collection)
-  .findOne({email: request.body.email});
-  response.render("reviewConf", result);
-});
-
-app.get("/adminGPA", (request, response) => {
-  response.render("adminGPA", {});
-});
-
-app.post("/processAdminGPA", async (request, response) => {
-  let table = "<table border=1>\n<tr><th>Name</th><th>GPA</th></tr>";
-  
-  let result = await client.db(databaseAndCollection.db)
-  .collection(databaseAndCollection.collection)
-  .find({gpa: {$gte: Number(request.body.gpa)}}).toArray();
-
-  result.forEach(item => {
-    table += `\n<tr><td>${item.name}</td><td>${item.gpa}</td></tr>`;
-  });
-  table += `\n</table>`;
-  response.render("adminGPAResult", {table: table});
-});
-
-app.get("/adminRemove", (request, response) => {
-  response.render("removeAll", {});
-});
-
-app.post("/processAdminRemove", async (request, response) => {
-  let result = await client.db(databaseAndCollection.db)
-  .collection(databaseAndCollection.collection)
-  .deleteMany({});
-  response.render("removeAllResult", {deleted: result.deletedCount});
-});
-
-// Command Line Interpreter
-console.log("Web server started and running at http://localhost:" + port);
-process.stdout.write("Stop to shutdown the server: ");
-process.stdin.setEncoding("utf8");
-process.stdin.on("readable", () => {
-  let command = process.stdin.read();
-  if (command !== null) {
-    command = command.trim();
-    if (command === "stop") {
-      console.log("Shutting down the server");
-      process.exit(0);
-    } else {
-      console.log("Invalid command: \"" + command + "\"");
-    }
-  }
-  process.stdout.write("Stop to shutdown the server: ");
-  process.stdin.resume();
-});
